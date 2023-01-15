11f3fd3a5d0561b25f4fc9ebc40ca68d6fd02b08
Kender ROMAIN <kenderromain@gmail.com>
2023-01-15 17:05:50 -0400

    Create shortcut to 'start' the webpack server and to' build' the bundle

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import _ from 'lodash';
+
+const component = () => {
+  const element = document.createElement('div');
+  element.innerHTML = _.join(['Hello', 'Webpack']);
+  return element;
+};
+
+document.body.appendChild(component());
